02526f701de1246d2b41852ffb38394aa74c1cef
Yue Cao <yue.cao@my.jcu.edu.au>
2022-01-04 21:57:37 +0800

    Add the kivy part for dynamic_labels.py

--- dynamic_labels.py
+++ dynamic_labels.py
@@ -1,5 +1,6 @@
 from kivy.app import App
 from kivy.lang import Builder
+from kivy.uix.label import Label
 from kivy.properties import StringProperty
 
 
@@ -14,6 +15,11 @@ class DynamicLabelsApp(App):
         self.title = "Dynamic Labels"
         self.root = Builder.load_file('dynamic_labels.kv')
         return self.root
+
+    def display_widgets(self):
+        for name in self.names:
+            temp_label = Label(text=name)
+            self.root.ids.main.add_widgets(temp_label)
 
 
 DynamicLabelsApp().run()
