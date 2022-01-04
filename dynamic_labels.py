bdbaa3a604c707b3c7894f2f27fe2780e8001ea3
Yue Cao <yue.cao@my.jcu.edu.au>
2022-01-04 22:15:28 +0800

    Delete the button part, and solve the bugs

--- dynamic_labels.py
+++ dynamic_labels.py
@@ -1,11 +1,9 @@
 from kivy.app import App
 from kivy.lang import Builder
 from kivy.uix.label import Label
-from kivy.properties import StringProperty
 
 
 class DynamicLabelsApp(App):
-    status_text = StringProperty()
 
     def __init__(self, **kwargs):
         super().__init__(**kwargs)
@@ -14,12 +12,13 @@ class DynamicLabelsApp(App):
     def build(self):
         self.title = "Dynamic Labels"
         self.root = Builder.load_file('dynamic_labels.kv')
+        self.display_widgets()
         return self.root
 
     def display_widgets(self):
         for name in self.names:
             temp_label = Label(text=name)
-            self.root.ids.main.add_widgets(temp_label)
+            self.root.ids.main.add_widget(temp_label)
 
 
 DynamicLabelsApp().run()
